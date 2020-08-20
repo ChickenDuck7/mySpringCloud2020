f337583ba2d695df25cfc02fdbdb16a4d2c03553
fanjh3 <fanjh3@asiainfo.com>
2020-08-20 22:02:22 +0800

    @第3次提交@fanjh3@20200820

--- cloud-consumer-order80/src/main/java/com/cd7/springcloud/controller/OrderContronller.java
+++ cloud-consumer-order80/src/main/java/com/cd7/springcloud/controller/OrderContronller.java
@@ -33,44 +33,46 @@ public class OrderContronller {
     private DiscoveryClient discoveryClient;
 
     @GetMapping("/consumer/payment/create")
-    public CommonResult<Payment> create(Payment payment)
-    {
-        return restTemplate.postForObject(PAYMENT_URL +"/payment/create",payment,CommonResult.class);
+    public CommonResult<Payment> create(Payment payment) {
+        return restTemplate.postForObject(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
     }
 
     @GetMapping("/consumer/payment/get/{id}")
-    public CommonResult<Payment> getPayment(@PathVariable("id") Long id)
-    {
-        return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
+    public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
+        return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
     }
 
     @GetMapping("/consumer/payment/getForEntity/{id}")
-    public CommonResult<Payment> getPayment2(@PathVariable("id") Long id)
-    {
-        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
+    public CommonResult<Payment> getPayment2(@PathVariable("id") Long id) {
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
 
-        if(entity.getStatusCode().is2xxSuccessful()){
+        if (entity.getStatusCode().is2xxSuccessful()) {
             return entity.getBody();
-        }else{
-            return new CommonResult<>(444,"操作失败");
+        } else {
+            return new CommonResult<>(444, "操作失败");
         }
     }
 
     @GetMapping(value = "/consumer/payment/lb")
-    public String getPaymentLB()
-    {
+    public String getPaymentLB() {
         List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
 
-        if(instances == null || instances.size() <= 0)
-        {
+        if (instances == null || instances.size() <= 0) {
             return null;
         }
 
         ServiceInstance serviceInstance = loadBalancer.instances(instances);
         URI uri = serviceInstance.getUri();
 
-        return restTemplate.getForObject(uri+"/payment/lb",String.class);
+        return restTemplate.getForObject(uri + "/payment/lb", String.class);
 
+    }
+
+    // ====================> zipkin+sleuth
+    @GetMapping("/consumer/payment/zipkin")
+    public String paymentZipkin() {
+        String result = restTemplate.getForObject("http://localhost:8001" + "/payment/zipkin/", String.class);
+        return result;
     }
 
 
